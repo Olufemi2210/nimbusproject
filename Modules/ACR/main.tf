7bea3a1e03c3733ee297169e37ea9fddf288d292
olufemi2210 <oduneyeolufemi22@gmail.com>
2026-08-14 14:11:05 +0100

    Fix ACR public network access SonarQube issue

--- Modules/ACR/main.tf
+++ Modules/ACR/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_container_registry" "example" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = "Premium"
+  name                          = var.name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  sku                           = "Premium"
+  public_network_access_enabled = false
 }
-
